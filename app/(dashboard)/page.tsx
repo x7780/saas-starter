9ddf1b78140d66616dd397834367e49c9891b6e1
huaihuai <x7780@163.com>
2025-05-17 21:56:53 +0800

    更新 page.tsx

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
         </section>
 
         {/* Features Section */}
-        <section id="features" className="w-full py-12 md:py-24 lg:py-32 flex flex-col justify-center items-center">
+        <section id="features" className="w-full py-12 md:py-24 lg:py-16 flex flex-col justify-center items-center">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
