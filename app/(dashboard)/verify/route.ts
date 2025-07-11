fc81dfc4dc9e35d5ca8c79afd1c1381bcba529d5
huaihuai <x7780@163.com>
2025-07-11 20:20:25 +0800

    更新 route.ts

--- app/(dashboard)/verify/route.ts
+++ app/(dashboard)/verify/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-const validCodes = ['abc123', 'test456', 'd9bda92937fbdf4ac36107547cb89cb9a874b93449c64c11d841c5ef3bf210c9']; // 允许的许可证
+const validCodes = ['abc123', 'test456', '5ad0f25bdfa74c08b1e3ad6a121ad052']; // 允许的许可证
 
 export async function GET(req: NextRequest) {
   const code = req.nextUrl.searchParams.get('code');
